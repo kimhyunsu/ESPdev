62c0600cea2b5e5681afd3ea70de45f827e6ac69
kimhyunsu <61767075+kimhyunsu@users.noreply.github.com>
2020-07-23 23:41:29 +0900

    2020.07.23

--- src/_P200_TFTLCD.ino
+++ src/_P200_TFTLCD.ino
@@ -30,7 +30,7 @@ TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
 #define TFT_CS              5
 #define TFT_DC              16
 #define TFT_RST             23
-#define TFT_BL              4   // Display backlight control pin
+#define TFT_BL              14   // Display backlight control pin
 
 #define P200_Nlines 2        // The number of different lines which can be displayed
 #define P200_Nchars 30
@@ -73,11 +73,11 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
     case PLUGIN_WEBFORM_LOAD:
       {
         byte choice1 = PCONFIG(0);
-        String options2[2];
-        options2[0] = F("0");
-        options2[1] = F("1");
-        int optionValues2[2] = { 0, 1 };
-        addFormSelector(F("Rotat"), F("p200_rotat"), 2, options2, optionValues2, choice1);
+        String options1[2];
+        options1[0] = F("0");
+        options1[1] = F("1");
+        int optionValues1[2] = { 0, 1 };
+        addFormSelector(F("Rotat"), F("p200_rotat"), 2, options1, optionValues1, choice1);
 
         byte choice2 = PCONFIG(1);
         String options2[9];
@@ -94,16 +94,16 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
         addFormSelector(F("Datum"), F("p200_datun"), 9, options2, optionValues2, choice2);
 
         byte choice3 = PCONFIG(2);
-        String options2[7];
-        options2[0] = F("glcdfont");
-        options2[1] = F("Font7srle");
-        options2[2] = F("Font16");
-        options2[3] = F("Font32rle");
-        options2[4] = F("Font64rle");
-        options2[5] = F("Font72rle");
-        options2[6] = F("Font72x53rle");
-        int optionValues2[7] = { 1, 2, 3, 4, 5, 6, 7 };
-        addFormSelector(F("Font"), F("p200_font"), 7, options2, optionValues2, choice3);
+        String options3[7];
+        options3[0] = F("glcdfont");
+        options3[1] = F("Font7srle");
+        options3[2] = F("Font16");
+        options3[3] = F("Font32rle");
+        options3[4] = F("Font64rle");
+        options3[5] = F("Font72rle");
+        options3[6] = F("Font72x53rle");
+        int optionValues3[7] = { 1, 2, 3, 4, 5, 6, 7 };
+        addFormSelector(F("Font"), F("p200_font"), 7, options3, optionValues3, choice3);
 
         {
           String strings[P200_Nlines];
@@ -127,9 +127,9 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
         // FIXME TD-er: This is a huge stack allocated object.
         char deviceTemplate[P200_Nlines][P200_Nchars];
         String error;
-        for (byte varNr = 0; varNr < P12_Nlines; varNr++)
+        for (byte varNr = 0; varNr < P200_Nlines; varNr++)
         {
-          if (!safe_strncpy(deviceTemplate[varNr], web_server.arg(getPluginCustomArgName(varNr)), P12_Nchars)) {
+          if (!safe_strncpy(deviceTemplate[varNr], web_server.arg(getPluginCustomArgName(varNr)), P200_Nchars)) {
             error += getCustomTaskSettingsError(varNr);
           }
         }
@@ -145,11 +145,13 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
       {
         tft.init();
         tft.setRotation(PCONFIG(0));
-        tft.fillScreen(TFT_BLACK);
+        tft.fillScreen(TFT_WHITE);
         tft.setTextSize(PCONFIG(2));
-        tft.setTextColor(TFT_GREEN);
-        tft.setCursor(0, 0);
+        tft.setTextColor(TFT_BLACK);
+        //tft.setCursor(20, 30);
         tft.setTextDatum(PCONFIG(1));
+        tft.drawString("TEMP : 26 c",210,50);
+        tft.drawString("HUM  : 56 %",210,100);
 
         if (TFT_BL > 0) {                           // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
         pinMode(TFT_BL, OUTPUT);                // Set backlight pin to output mode
@@ -162,18 +164,14 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
     case PLUGIN_READ:
       {
         // FIXME TD-er: This is a huge stack allocated object.
-        char deviceTemplate[P12_Nlines][P12_Nchars];
+        char deviceTemplate[P200_Nlines][P200_Nchars];
         LoadCustomTaskSettings(event->TaskIndex, (byte*)&deviceTemplate, sizeof(deviceTemplate));
 
-        for (byte x = 0; x < Plugin_012_rows; x++)
+        for (byte x = 0; x < 2; x++)
         {
           String tmpString = deviceTemplate[x];
-          if (lcd && tmpString.length())
-          {
-            String newString = P012_parseTemplate(tmpString, Plugin_012_cols);
-            lcd->setCursor(0, x);
-            lcd->print(newString);
-          }
+
+
         }
         success = false;
         break;
@@ -181,119 +179,14 @@ boolean Plugin_200(byte function, struct EventStruct *event, String& string)
 
     case PLUGIN_WRITE:
       {
-        String cmd = parseString(string, 1);
-        if (lcd && cmd.equalsIgnoreCase(F("LCDCMD")))
-        {
-          success = true;
-          String arg1 = parseString(string, 2);
-          if (arg1.equalsIgnoreCase(F("Off"))){
-              lcd->noBacklight();
-          }
-          else if (arg1.equalsIgnoreCase(F("On"))){
-              lcd->backlight();
-          }
-          else if (arg1.equalsIgnoreCase(F("Clear"))){
-              lcd->clear();
-          }
-        }
-        else if (lcd && cmd.equalsIgnoreCase(F("LCD")))
-        {
-          success = true;
-          int colPos = event->Par2 - 1;
-          int rowPos = event->Par1 - 1;
-          String text = parseStringKeepCase(string, 4);
-          text = P012_parseTemplate(text, Plugin_012_cols);
 
-          //clear line before writing new string
-          if (Plugin_012_mode == 2){
-              lcd->setCursor(colPos, rowPos);
-              for (byte i = colPos; i < Plugin_012_cols; i++) {
-                  lcd->print(" ");
-              }
-          }
 
-          // truncate message exceeding cols
-          lcd->setCursor(colPos, rowPos);
-          if(Plugin_012_mode == 1 || Plugin_012_mode == 2){
-              lcd->setCursor(colPos, rowPos);
-              for (byte i = 0; i < Plugin_012_cols - colPos; i++) {
-                  if(text[i]){
-                     lcd->print(text[i]);
-                  }
-              }
-          }
 
-          // message exceeding cols will continue to next line
-          else{
-              // Fix Weird (native) lcd display behaviour that split long string into row 1,3,2,4, instead of 1,2,3,4
-              boolean stillProcessing = 1;
-              byte charCount = 1;
-              while(stillProcessing) {
-                   if (++colPos > Plugin_012_cols) {    // have we printed 20 characters yet (+1 for the logic)
-                        rowPos += 1;
-                        lcd->setCursor(0,rowPos);   // move cursor down
-                        colPos = 1;
-                   }
-
-                   //dont print if "lower" than the lcd
-                   if(rowPos < Plugin_012_rows  ){
-                       lcd->print(text[charCount - 1]);
-                   }
-
-                   if (!text[charCount]) {   // no more chars to process?
-                        stillProcessing = 0;
-                   }
-                   charCount += 1;
-              }
-              //lcd->print(text.c_str());
-              // end fix
-          }
-
-        }
-        break;
       }
 
   }
   return success;
 }
 
-// Perform some specific changes for LCD display
-// https://www.letscontrolit.com/forum/viewtopic.php?t=2368
-String P012_parseTemplate(String &tmpString, byte lineSize) {
-  String result = parseTemplate_padded(tmpString, lineSize);
-  const char degree[3] = {0xc2, 0xb0, 0};  // Unicode degree symbol
-  const char degree_lcd[2] = {0xdf, 0};  // P012_LCD degree symbol
-  result.replace(degree, degree_lcd);
 
-  char unicodePrefix = 0xc3;
-  if (result.indexOf(unicodePrefix) != -1) {
-    // See: https://github.com/letscontrolit/ESPEasy/issues/2081
-
-    const char umlautAE_uni[3] = {0xc3, 0x84, 0}; // Unicode Umlaute AE
-    const char umlautAE_lcd[2] = {0xe1, 0}; // P012_LCD Umlaute
-    result.replace(umlautAE_uni, umlautAE_lcd);
-
-    const char umlaut_ae_uni[3] = {0xc3, 0xa4, 0}; // Unicode Umlaute ae
-    result.replace(umlaut_ae_uni, umlautAE_lcd);
-
-    const char umlautOE_uni[3] = {0xc3, 0x96, 0}; // Unicode Umlaute OE
-    const char umlautOE_lcd[2] = {0xef, 0}; // P012_LCD Umlaute
-    result.replace(umlautOE_uni, umlautOE_lcd);
-
-    const char umlaut_oe_uni[3] = {0xc3, 0xb6, 0}; // Unicode Umlaute oe
-    result.replace(umlaut_oe_uni, umlautOE_lcd);
-
-    const char umlautUE_uni[3] = {0xc3, 0x9c, 0}; // Unicode Umlaute UE
-    const char umlautUE_lcd[2] = {0xf5, 0}; // P012_LCD Umlaute
-    result.replace(umlautUE_uni, umlautUE_lcd);
-
-    const char umlaut_ue_uni[3] = {0xc3, 0xbc, 0}; // Unicode Umlaute ue
-    result.replace(umlaut_ue_uni, umlautUE_lcd);
-
-    const char umlaut_sz_uni[3] = {0xc3, 0x9f, 0}; // Unicode Umlaute sz
-    const char umlaut_sz_lcd[2] = {0xe2, 0}; // P012_LCD Umlaute
-    result.replace(umlaut_sz_uni, umlaut_sz_lcd);
-  }
-  return result;
-}
 #endif // USES_P012
